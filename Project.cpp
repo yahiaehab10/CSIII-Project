2229b12366c355b2300fca9a6df3626bd09084cf
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2022-11-14 20:00:04 +0200

    First commit

---
+++ Project.cpp
@@ -0,0 +1,38 @@
+//Team 6
+
+#include <iostream>
+using namespace std;
+
+int main(){}
+
+//Champiom
+class champion{
+private:
+    /* data */
+public:
+    champion(/* args */);
+    ~champion();
+};
+
+champion::champion(/* args */){
+}
+
+champion::~champion(){
+}
+
+//Map
+class map{
+private:
+    /* data */
+public:
+    map(/* args */);
+    ~map();
+};
+
+map::map(/* args */){
+}
+
+map::~map(){
+}
+
+
